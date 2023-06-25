ab3c84eaec2240336def7206455cc64d9e79ffc0
Gene Calm <ecalmrd@gmail.com>
2023-06-24 21:26:24 -0700

    added js code in generator.js

--- src/pages/Generator.js
+++ src/pages/Generator.js
@@ -1,119 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import '../App.css'
 
+export default function Generator() {
+    // Variables for password criteria
+    const alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    const alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    const numerics = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    const specialChar = ["!", "@", "#", "$", "%", "^", "&", ",", "*", "(", ")", ",", "-", "_", "=", "+", "-", ".", "/", "<", ">", "?", "~"];
 
-const styles = {
-    section: {
-        backgroundColor: '#151722',
-        height: '100vh',
-        padding: '1px',
-    },
-    center: {
-        backgroundColor: '#28282B',
-        textAlign: 'center',
-        justifyContent: 'center',
-        alignItems: 'center',
-        border: 'solid',
-        borderRadius: '10px',
-        height: '400px',
-        width: '800px',
-        paddingTop: '20px',
-        margin: 'auto',
-    },
-    text: {
-        fontFamily: 'Orbitron',
-        fontWeight: 'bold',
-        color: 'dark gray',
-    },
-    button: {
-        fontFamily: 'Orbitron',
-        color: '#F9F6EE',
-        backgroundColor: '#36454F',
-        borderRadius: '10px',
-        padding: '10px',
-        margin: '10px',
-    },
-    textbox: {
-        backgroundColor: '#f8fbf8',
-        fontFamily: 'Orbitron',
-        color: 'black',
-        height: '50px',
-        width: '550px',
-        borderRadius: '10px',
-        padding: '10px',
-        margin: '10px',
-        textAlign: 'center',
-        resize: 'none',
-    },
-    form: {
-        display: 'grid',
-        gridTemplateColumns: 'repeat(3, 1fr)',
-        gridGap: '10px',
-        fontFamily: 'Orbitron',
-        color: '#f8fbf8',
-        borderRadius: '10px',
-        padding: '10px',
-        margin: '10px',
-        textAlign: 'center',
-    },
-    cardTitle: {
-        fontFamily: 'Orbitron',
-        objectFit: 'contain',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'bottom center',
-        justifyContent: 'flex-end',
-        color: '#f8fbf8',
+    // Password prompts
+    const [length, setLength] = useState(8);
+    const [lowercase, setLowercase] = useState(false);
+    const [uppercase, setUppercase] = useState(false);
+    const [numbers, setNumbers] = useState(false);
+    const [special, setSpecial] = useState(false);
+    const [password, setPassword] = useState('');
+
+    function handleChange(event) {
+        setLength(event.target.value);
     }
-}
 
-function Generator() {
+    function generatePassword() {
+        const length = parseInt(document.getElementById('password').value);
+        const lowercase = document.getElementById('lowerC').checked;
+        const uppercase = document.getElementById('upperC').checked;
+        const numbers = document.getElementById('numbers').checked;
+        const special = document.getElementById('special').checked;
+
+        let passCriteria = "";
+        if (lowercase) passCriteria += alphaLower;
+        if (uppercase) passCriteria += alphaUpper;
+        if (numbers) passCriteria += numerics;
+        if (special) passCriteria += specialChar;
+
+        let password = "";
+        for (let i = 0; i < length; i++) {
+            password = password + passCriteria[Math.floor(Math.random() * passCriteria.length)];
+            console.log(password);
+        }
+        setPassword(password);
+    }
+
+
     return (
-        <section className='section' style={styles.section}>
-            <div className="wrapper" style={styles.center}>
-                <div className="card-header">
-                    <h2 className='app' style={styles.cardTitle}>Generate Your Password</h2>
+        <section className='section'>
+            <div className='container'>
+                <div className='card-header'>
+                    <h2 className='cardTitle'>Generate Your Password</h2>
                 </div>
                 <div className="card">
                     <div className="card-body" >
                         <textarea
-                            style={styles.textbox}
+                            className='textBox'
                             id="password"
                             placeholder="Your Secure Password"
                             aria-label="Generated Password"
                         ></textarea>
                     </div>
 
-                    <form className='form' style={styles.form}>
+                    <form className='form formGrid'>
                         <label>
-                            <input type="checkbox" value="" />item1
+                            <input type="number" id="length" min='8' max='24' value={length} onChange={handleChange} />length
                         </label>
                         <label>
-                            <input type="checkbox" value="" />item2
+                            <input type="checkbox" id="lowercase" checked={lowercase} onChange={(e) => setLowercase(e.target.checked)} /> lowercase
                         </label>
                         <label>
-                            <input type="checkbox" value="" />item3
+                            <input type="checkbox" id="uppercase" checked={uppercase} onChange={(e) => setUppercase(e.target.checked)} /> uppercase
                         </label>
                         <label>
-                            <input type="checkbox" value="" />item4
+                            <input type="checkbox" id="numbers" checked={numbers} onChange={(e) => setNumbers(e.target.checked)} /> numbers
                         </label>
                         <label>
-                            <input type="checkbox" value="" />item5
-                        </label>
-                        <label>
-                            <input type="checkbox" value="" />item6
-                        </label>
-                        <label>
-                            <input type="checkbox" value="" />item7
-                        </label>
-                        <label>
-                            <input type="checkbox" value="" />item8
-                        </label>
-                        <label>
-                            <input type="checkbox" value="" />item9
+                            <input type="checkbox" id="special" checked={special} onChange={(e) => setSpecial(e.target.checked)} /> special characters
                         </label>
                     </form>
                     <div className="card-footer">
-                        <button className="generatebtn" style={styles.button}>Generate Password</button>
+                        <button className="generateButton" id="generateBtn" onClick={() => {
+                            const passwordText = document.querySelector("#password");
+                            passwordText.value = generatePassword();
+                        }}>Generate Password</button>
                     </div>
                 </div>
             </div>
@@ -122,4 +89,4 @@ function Generator() {
     );
 }
 
-export default Generator;
+
